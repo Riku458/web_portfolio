aa776349e0e9398c69410b75d55c507b62508025
Riku458 <jedrickpogi69@gmail.com>
2025-10-28 20:00:18 +0800

    Making a fuctioning operation of infix_to_postfix expression and also adding design to its html

--- app.py
+++ app.py
@@ -55,5 +55,42 @@ def work_triangle():
 def contacts():
     return render_template('contacts.html')
 
+@app.route('/works/infix_to_postfix', methods=['GET', 'POST'])
+def work_infix_to_postfix():
+    result = None
+    if request.method == 'POST':
+        expression = request.form.get('expression', '')
+
+        def infix_to_postfix(expr):
+            precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+            output = []
+            stack = []
+            tokens = list(expr.replace(" ", ""))
+
+            for token in tokens:
+                if token.isalnum():
+                    output.append(token)
+                elif token == '(':
+                    stack.append(token)
+                elif token == ')':
+                    while stack and stack[-1] != '(':
+                        output.append(stack.pop())
+                    stack.pop()
+                else:
+                    while stack and stack[-1] != '(' and precedence.get(stack[-1], 0) >= precedence.get(token, 0):
+                        output.append(stack.pop())
+                    stack.append(token)
+
+            while stack:
+                output.append(stack.pop())
+
+            return ''.join(output)
+
+        if expression:
+            result = infix_to_postfix(expression)
+
+    return render_template('work_infix_to_postfix.html', result=result)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
